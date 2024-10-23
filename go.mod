19b5afdb3ecf0a62f130558cadb444e7c13cd19c
portfolioaidiladam <portfolioaidiladam@gmail.com>
2024-10-23 10:13:27 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/portfolioaidiladam/Projectminigolangmodule2
+
+go 1.23.2
+
+require github.com/portfolioaidiladam/Projectminigolangmodule1 v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+import (
+	"fmt"
+	go_say_hello "github.com/portfolioaidiladam/Projectminigolangmodule1"
+)
+func main() {
+	fmt.Println(go_say_hello.SayHello("Aidil"))
+}
